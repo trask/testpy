0360ba5b6c6d37e36858be1b3240e03d6314b36c
trask <trask@users.noreply.github.com>
2022-04-26 03:59:16 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="trask"
+original_name="testpy"
+original_urlname="testpy"
+original_description="Awesome testpy created by trask"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv testpy $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
